7db6b3fffe9310abb0ad2b3dd59e6e418c376055
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-07-20 19:59:40 +0200

    initial commit

---
+++ src/main/java/com/apiangulargallery/apiangulargallery/ApiAngularGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.apiangulargallery.apiangulargallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAngularGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAngularGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiangulargallery/apiangulargallery/ApiAngularGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiangulargallery.apiangulargallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAngularGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
