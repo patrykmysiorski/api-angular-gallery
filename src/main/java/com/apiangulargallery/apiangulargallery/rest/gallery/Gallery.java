95685d45bee9b3be26caf4199b162d5a40c32dcd
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-07-21 22:05:02 +0200

    create gallery model

--- src/main/java/com/apiangulargallery/apiangulargallery/rest/gallery/Gallery.java
+++ src/main/java/com/apiangulargallery/apiangulargallery/rest/gallery/Gallery.java
@@ -1,5 +1,6 @@
 package com.apiangulargallery.apiangulargallery.rest.gallery;
 
+import com.apiangulargallery.apiangulargallery.rest.photo.Photo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,6 +19,11 @@ import javax.persistence.GenerationType;
 public class Gallery {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id;
-    private String galleryName;
+    private String galleryId;
+    private String title;
+    private String description;
+    private String dateCreated;
+    private String thumbUrl;
+    private List<String> tags;
+    private List<Photo> photos;
 }
